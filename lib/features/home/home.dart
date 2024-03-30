d7b7aa2b0749122ff5a591e3ee8bc123ac317146
Yura-0 <yurec.name@gmail.com>
2024-03-30 15:45:58 +0200

    add theme button

--- lib/features/home/home.dart
+++ lib/features/home/home.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:itracker/core/app/injector.dart';
 
+import '../../core/blocs/theme_bloc/theme_cubit.dart';
 import 'botom_bar_widget.dart';
 
 class HomePage extends StatelessWidget {
- const HomePage({super.key});
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     PageController pageController = PageController();
     return Scaffold(
+      appBar: AppBar(
+        actions: [
+          BlocBuilder<ThemeCubit, ThemeState>(
+            builder: (context, state) {
+              return IconButton(
+                onPressed: () {
+                  locator<ThemeCubit>().toggleTheme();
+                },
+                icon: state == ThemeState.light
+                    ? const Icon(Icons.dark_mode)
+                    : const Icon(Icons.light_mode),
+              );
+            },
+          )
+        ],
+      ),
       body: PageView(
         controller: pageController,
         children: const [
@@ -21,6 +40,5 @@ class HomePage extends StatelessWidget {
         pageController: pageController,
       ),
     );
-  
   }
 }
